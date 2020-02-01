ac907fc9cade7a6da6146ebd4060cbbc0078fdd1
park byeong kyu <pbk4008@naver.com>
2020-02-01 12:02:21 +0900

    no message

---
+++ SpineSurgery/Assets/Scripts/Pbk/Cmouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cmouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform tr;
+    void Start()
+    {
+        tr = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.position = Input.mousePosition;
+    }
+}
