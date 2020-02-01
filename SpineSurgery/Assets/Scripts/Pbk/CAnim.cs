11225a245b7566992b67e9682e5206e4e47996b8
park byeong kyu <pbk4008@naver.com>
2020-02-01 16:56:03 +0900

    팔 애니메이션 추가

---
+++ SpineSurgery/Assets/Scripts/Pbk/CAnim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Animator Left;
+    [SerializeField]
+    private Animator Right;
+    void Start()
+    {
+        StartCoroutine(setAnimNum());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator setAnimNum()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(5.0f);
+            int res1 = Random.Range(0, 3);
+            int res2 = Random.Range(0, 3);
+            Debug.Log(res1);
+            Debug.Log(res2);
+            Left.SetInteger("LArmSwitch", res1);
+            Right.SetInteger("RArmSwitch", res2);
+        }
+    }
+}
